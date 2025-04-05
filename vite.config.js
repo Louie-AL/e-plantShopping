f0401ea109a36ddd139d8e2cd00ac3e421b2dd9d
LuisCV <your-email@example.com>
2025-04-05 03:29:33 -0400

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/e-plantShopping',  // <-- your repo name here
+  base: '/e-plantShopping/',  // ✅ with trailing slash
   plugins: [react()],
-})
+});
+
